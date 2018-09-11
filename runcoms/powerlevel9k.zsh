6d8a786b5e8dc780fc1de793668dea0684e28665
Peter Tillemans <pti@melexis.com>
2018-09-11 17:18:14 +0200

    add support to print execution times

--- runcoms/powerlevel9k.zsh
+++ runcoms/powerlevel9k.zsh
@@ -3,8 +3,9 @@
 # PowerLevel9K configuration
 POWERLEVEL9K_MODE='nerdfont-complete'
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode os_icon context dir newline vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon battery load ram context vcs newline dir)
+
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time status root_indicator background_jobs history time)
 
 POWERLEVEL9K_PROMPT_ON_NEWLINE=false
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
@@ -19,22 +20,11 @@ POWERLEVEL9K_VI_MODE_NORMAL_BACKGROUND='yellow'
 
 DEFAULT_USER="pti"
 
+POWERLEVEL9K_BATTERY_VERBOSE=false
+
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
 POWERLEVEL9K_SHORTEN_DELIMITER=""
 POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
 
-
-# change cursor shape
-zle-keymap-select () {
-  if [ "$TERM" = "xterm-256color" ]; then
-    if [ $KEYMAP = vicmd ]; then
-      # the command mode for vi
-      echo -ne "\e[2 q"
-    else
-      # the insert mode for vi
-      echo -ne "\e[5 q"
-    fi
-  fi
-  zle reset-prompt
-  zle -R
-}
+POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
+POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=2
