6e1167066e01654445b18eb721cd2d89fca00f6e
Peter Tillemans <pti@melexis.com>
2018-09-11 14:23:54 +0200

    enable powerlevel9k prompt

---
+++ runcoms/powerlevel9k.zsh
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# PowerLevel9K configuration
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode os_icon context dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_VCS_GIT_BITBUCKET_ICON=
+POWERLEVEL9K_VCS_GIT_GITHUB_ICON=
+POWERLEVEL9K_VCS_GIT_GITLAB_ICON=
+POWERLEVEL9K_VCS_GIT_ICON=
+
+POWERLEVEL9K_VI_INSERT_MODE_STRING=""
+POWERLEVEL9K_VI_COMMAND_MODE_STRING=""
+# Advanced `vi_mode` color customization
+POWERLEVEL9K_VI_MODE_INSERT_FOREGROUND='black'
+POWERLEVEL9K_VI_MODE_INSERT_BACKGROUND='green'
+POWERLEVEL9K_VI_MODE_NORMAL_FOREGROUND='teal'
+POWERLEVEL9K_VI_MODE_NORMAL_BACKGROUND='yellow'
+
+DEFAULT_USER="pti"
+
+POWERLEVEL9K_APPLE_ICON=""
+POWERLEVEL9K_LINUX_ICON=""
+POWERLEVEL9K_LINUX_UBUNTU_ICON=""
+POWERLEVEL9K_LINUX_ARCH_ICON=""
+
+# change cursor shape
+zle-keymap-select () {
+  if [ "$TERM" = "xterm-256color" ]; then
+    if [ $KEYMAP = vicmd ]; then
+      # the command mode for vi
+      echo -ne "\e[2 q"
+    else
+      # the insert mode for vi
+      echo -ne "\e[5 q"
+    fi
+  fi
+  zle reset-prompt
+  zle -R
+}
